7f43af993d325fca48d554ffbc00f7f2d72e0f25
Johnny Law <mingzhang666@outlook.com>
2022-08-04 15:20:17 +0800

    initial

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,33 @@
+using System.Runtime.InteropServices;
+
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("97a154b8-fea4-4514-955f-2481ced1770b")]
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
